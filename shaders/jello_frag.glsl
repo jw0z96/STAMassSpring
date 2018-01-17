16b09306c8d59fa23e9c4b6b988ddf2bc590da3a
Joe Withers <joewithers96@gmail.com>
2018-01-17 01:32:51 +0000

    working basic shading

--- shaders/jello_frag.glsl
+++ shaders/jello_frag.glsl
@@ -5,20 +5,32 @@ layout (location=0) out vec4 fragColor;
 
 flat in int axis;
 
+smooth in vec3 f_normal;
+smooth in vec3 f_pos;
+
+uniform vec3 camPos;
+
 void main()
 {
+	vec3 lightPos = vec3(10.0);
+	vec3 L = normalize(lightPos - f_pos);
+	float NdotL = max(0.0, dot(L, f_normal));
+	float LdotV = max(0.0, dot(normalize(camPos - f_pos), reflect(f_normal, L)));
+	vec3 diffuse = vec3(0.0);
+
 	if (axis == 0)
-		fragColor = vec4(1.0, 0.0, 0.0, 0.0);
+		diffuse = vec3(1.0, 0.0, 0.0);
 	else if (axis == 1)
-		fragColor = vec4(0.0, 1.0, 0.0, 0.0);
+		diffuse = vec3(0.0, 1.0, 0.0);
 	else if (axis == 2)
-		fragColor = vec4(0.0, 0.0, 1.0, 0.0);
+		diffuse = vec3(0.0, 0.0, 1.0);
 	else if (axis == 3)
-		fragColor = vec4(1.0, 1.0, 0.0, 0.0);
+		diffuse = vec3(1.0, 1.0, 0.0);
 	else if (axis == 4)
-		fragColor = vec4(1.0, 0.0, 1.0, 0.0);
+		diffuse = vec3(1.0, 0.0, 1.0);
 	else if (axis == 5)
-		fragColor = vec4(1.0, 1.0, 1.0, 0.0);
-	else
-		fragColor = vec4(0.0);
+		diffuse = vec3(1.0, 1.0, 1.0);
+
+	fragColor = vec4(diffuse * (LdotV + NdotL), 0.0);
+	// fragColor = vec4(f_pos, 0.0);
 }
--- shaders/jello_geo.glsl
+++ shaders/jello_geo.glsl
@@ -24,6 +24,9 @@ uniform mat4 MVP;
 
 flat out int axis;
 
+smooth out vec3 f_normal;
+smooth out vec3 f_pos;
+
 int getIndex(ivec3 pos)
 {
 	return int(
@@ -33,10 +36,26 @@ int getIndex(ivec3 pos)
 		);
 }
 
+// void emitQuad(vec4 _verts[4])
+// {
+// 	// calculate the normal for our first triangle in the quad;
+// 	// vec3 wsNormal = normalize(
+// 	// 	cross(_verts[1].xyz - _verts[0].xyz, _verts[2].xyz - _verts[0].xyz) +
+// 	// 	cross(_verts[2].xyz - _verts[1].xyz, _verts[3].xyz - _verts[1].xyz));
+
+// 	for (int i = 0; i < 4; ++i)
+// 	{
+// 		// f_normal = wsNormal;
+// 		// f_pos = _verts[i].xyz;
+// 		gl_Position = MVP * _verts[i];
+// 		EmitVertex();
+// 	}
+// 	EndPrimitive;
+// }
+
 void main()
 {
 	int quadID = gl_PrimitiveIDIn;
-
 	int quadsXY = (u_sizeX - 1) * (u_sizeY - 1);
 	int quadsXZ = (u_sizeX - 1) * (u_sizeZ - 1);
 	int quadsYZ = (u_sizeY - 1) * (u_sizeZ - 1);
@@ -47,84 +66,121 @@ void main()
 		int i = int(mod(quadID, (u_sizeX - 1)));
 		int j = int(floor((quadID - i) / (u_sizeY - 1)));
 		axis = 0;
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, j, 0))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, j + 1, 0))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, j, 0))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, j + 1, 0))].position.xyz, 1.0);
-		EmitVertex();
+		vec4 vertices[4];
+		vertices[0] = vec4(masses[getIndex(ivec3(i, j, 0))].position.xyz, 1.0);
+		vertices[1] = vec4(masses[getIndex(ivec3(i, j + 1, 0))].position.xyz, 1.0);
+		vertices[2] = vec4(masses[getIndex(ivec3(i + 1, j, 0))].position.xyz, 1.0);
+		vertices[3] = vec4(masses[getIndex(ivec3(i + 1, j + 1, 0))].position.xyz, 1.0);
+		vec3 wsNormal = normalize(
+			cross(vertices[1].xyz - vertices[0].xyz, vertices[2].xyz - vertices[0].xyz) +
+			cross(vertices[1].xyz - vertices[2].xyz, vertices[3].xyz - vertices[2].xyz));
+		for (int i = 0; i < 4; ++i)
+		{
+			f_normal = wsNormal;
+			f_pos = vertices[i].xyz;
+			gl_Position = MVP * vertices[i];
+			EmitVertex();
+		}
 		EndPrimitive();
 		// and for the opposite side
 		axis = 1;
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, j, (u_sizeZ - 1)))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, j, (u_sizeZ - 1)))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, j + 1, (u_sizeZ - 1)))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, j + 1, (u_sizeZ - 1)))].position.xyz, 1.0);
-		EmitVertex();
+		vertices[0] = vec4(masses[getIndex(ivec3(i, j, (u_sizeZ - 1)))].position.xyz, 1.0);
+		vertices[1] = vec4(masses[getIndex(ivec3(i + 1, j, (u_sizeZ - 1)))].position.xyz, 1.0);
+		vertices[2] = vec4(masses[getIndex(ivec3(i, j + 1, (u_sizeZ - 1)))].position.xyz, 1.0);
+		vertices[3] = vec4(masses[getIndex(ivec3(i + 1, j + 1, (u_sizeZ - 1)))].position.xyz, 1.0);
+		wsNormal = normalize(
+			cross(vertices[1].xyz - vertices[0].xyz, vertices[2].xyz - vertices[0].xyz) +
+			cross(vertices[1].xyz - vertices[2].xyz, vertices[3].xyz - vertices[2].xyz));
+		for (int i = 0; i < 4; ++i)
+		{
+			f_normal = wsNormal;
+			f_pos = vertices[i].xyz;
+			gl_Position = MVP * vertices[i];
+			EmitVertex();
+		}
 		EndPrimitive();
 	}
-	// draw all of the Y - Z quads
-	if (quadID < quadsXY + quadsYZ)
+	else if (quadID < quadsXY + quadsYZ) // draw all of the Y - Z quads
 	{
 		int id = quadID - quadsXY;
 		int i = int(mod(id, (u_sizeZ - 1)));
 		int j = int(floor((id - i) / (u_sizeY - 1)));
-
 		axis = 2;
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(0, i, j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(0, i, j + 1))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(0, i + 1, j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(0, i + 1, j + 1))].position.xyz, 1.0);
-		EmitVertex();
+		vec4 vertices[4];
+		vertices[0] = vec4(masses[getIndex(ivec3(0, i, j))].position.xyz, 1.0);
+		vertices[1] = vec4(masses[getIndex(ivec3(0, i, j + 1))].position.xyz, 1.0);
+		vertices[2] = vec4(masses[getIndex(ivec3(0, i + 1, j))].position.xyz, 1.0);
+		vertices[3] = vec4(masses[getIndex(ivec3(0, i + 1, j + 1))].position.xyz, 1.0);
+		vec3 wsNormal = normalize(
+			cross(vertices[1].xyz - vertices[0].xyz, vertices[2].xyz - vertices[0].xyz) +
+			cross(vertices[1].xyz - vertices[2].xyz, vertices[3].xyz - vertices[2].xyz));
+		for (int i = 0; i < 4; ++i)
+		{
+			f_normal = wsNormal;
+			f_pos = vertices[i].xyz;
+			gl_Position = MVP * vertices[i];
+			EmitVertex();
+		}
 		EndPrimitive();
 		// and for the opposite side
 		axis = 3;
-		gl_Position = MVP * vec4(masses[getIndex(ivec3((u_sizeX - 1), i, j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3((u_sizeX - 1), i + 1, j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3((u_sizeX - 1), i, j + 1))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3((u_sizeX - 1), i + 1, j + 1))].position.xyz, 1.0);
-		EmitVertex();
+		vertices[0] = vec4(masses[getIndex(ivec3((u_sizeX - 1), i, j))].position.xyz, 1.0);
+		vertices[1] = vec4(masses[getIndex(ivec3((u_sizeX - 1), i + 1, j))].position.xyz, 1.0);
+		vertices[2] = vec4(masses[getIndex(ivec3((u_sizeX - 1), i, j + 1))].position.xyz, 1.0);
+		vertices[3] = vec4(masses[getIndex(ivec3((u_sizeX - 1), i + 1, j + 1))].position.xyz, 1.0);
+		wsNormal = normalize(
+			cross(vertices[1].xyz - vertices[0].xyz, vertices[2].xyz - vertices[0].xyz) +
+			cross(vertices[1].xyz - vertices[2].xyz, vertices[3].xyz - vertices[2].xyz));
+		for (int i = 0; i < 4; ++i)
+		{
+			f_normal = wsNormal;
+			f_pos = vertices[i].xyz;
+			gl_Position = MVP * vertices[i];
+			EmitVertex();
+		}
 		EndPrimitive();
 	}
-	// draw all of the X - Z quads
-	else
+	else if (quadID < quadsXY + quadsYZ + quadsXZ) // draw all of the X - Z quads
 	{
 		int id = quadID - (quadsXY + quadsYZ);
-
 		int i = int(mod(id, (u_sizeX - 1)));
 		int j = int(floor((id - i) / (u_sizeZ - 1)));
-
 		axis = 4;
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, 0, j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, 0, j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, 0, j + 1))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, 0, j + 1))].position.xyz, 1.0);
-		EmitVertex();
+		vec4 vertices[4];
+		vertices[0] = vec4(masses[getIndex(ivec3(i, 0, j))].position.xyz, 1.0);
+		vertices[1] = vec4(masses[getIndex(ivec3(i + 1, 0, j))].position.xyz, 1.0);
+		vertices[2] = vec4(masses[getIndex(ivec3(i, 0, j + 1))].position.xyz, 1.0);
+		vertices[3] = vec4(masses[getIndex(ivec3(i + 1, 0, j + 1))].position.xyz, 1.0);
+		vec3 wsNormal = normalize(
+			cross(vertices[1].xyz - vertices[0].xyz, vertices[2].xyz - vertices[0].xyz) +
+			cross(vertices[1].xyz - vertices[2].xyz, vertices[3].xyz - vertices[2].xyz));
+		for (int i = 0; i < 4; ++i)
+		{
+			f_normal = wsNormal;
+			f_pos = vertices[i].xyz;
+			gl_Position = MVP * vertices[i];
+			EmitVertex();
+		}
 		EndPrimitive();
 		// and for the opposite side
 		axis = 5;
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, (u_sizeY - 1), j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i, (u_sizeY - 1), j + 1))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, (u_sizeY - 1), j))].position.xyz, 1.0);
-		EmitVertex();
-		gl_Position = MVP * vec4(masses[getIndex(ivec3(i + 1, (u_sizeY - 1), j + 1))].position.xyz, 1.0);
-		EmitVertex();
+		vertices[0] = vec4(masses[getIndex(ivec3(i, (u_sizeY - 1), j))].position.xyz, 1.0);
+		vertices[1] = vec4(masses[getIndex(ivec3(i, (u_sizeY - 1), j + 1))].position.xyz, 1.0);
+		vertices[2] = vec4(masses[getIndex(ivec3(i + 1, (u_sizeY - 1), j))].position.xyz, 1.0);
+		vertices[3] = vec4(masses[getIndex(ivec3(i + 1, (u_sizeY - 1), j + 1))].position.xyz, 1.0);
+		wsNormal = normalize(
+			cross(vertices[1].xyz - vertices[0].xyz, vertices[2].xyz - vertices[0].xyz) +
+			cross(vertices[1].xyz - vertices[2].xyz, vertices[3].xyz - vertices[2].xyz));
+		for (int i = 0; i < 4; ++i)
+		{
+			f_normal = wsNormal;
+			f_pos = vertices[i].xyz;
+			gl_Position = MVP * vertices[i];
+			EmitVertex();
+		}
 		EndPrimitive();
+	}
+	else
+	{
 	}
 }
